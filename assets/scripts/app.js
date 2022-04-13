d36a7b85ad31152849239c59795638741ca5a684
Alex Asriyan <aasriyan01@gmail.com>
2022-04-13 09:11:12 -0400

    introduced inheritance

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,23 +12,64 @@ class Product {
 	}
 }
 
-class ShoppingCart {
+class ElementAttribute {
+	constructor(attrName, attrValue) {
+		this.name = attrName;
+		this.value = attrValue;
+	}
+}
+class Component {
+	constructor(renderHookId) {
+		this.hookId = renderHookId;
+	}
+	createRootElement(tag, cssClasses, attributes) {
+		const rootElement = document.createElement(tag);
+		if (cssClasses) {
+			rootElement.className = cssClasses;
+		}
+
+		if (attributes && attributes.length > 0) {
+			for (const attr of attributes) {
+				rootElement.setAttribute(attr.name, attr.value);
+			}
+		}
+
+		document.getElementById(this.hookId).append(rootElement);
+		return rootElement;
+	}
+}
+
+class ShoppingCart extends Component {
 	items = [];
 
+	set cartItems(value) {
+		this.items = value;
+		this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+	}
+
+	get totalAmount() {
+		const sum = this.items.reduce((prevValue, curItem) => prevValue + curItem.price, 0);
+		return sum;
+	};
+
+	constructor(renderHookId) {
+		super(renderHookId);
+	}
+
 	addProduct(product) {
-		this.items.push(product);
-		this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+		const updatedItems = [...this.items];
+		updatedItems.push(product);
+		this.cartItems = updatedItems;
 	}
 
 	render() {
-		const cartEl = document.createElement("section");
+		const cartEl = this.createRootElement("section", "cart");
 		cartEl.innerHTML = `
 			<h2>Total: \$${0}</h2>
 			<button>Order Now!</button>
 		`;
 		cartEl.className = "cart";
 		this.totalOutput = cartEl.querySelector("h2");
-		return cartEl;
 	}
 }
 
@@ -63,8 +104,8 @@ class ProductItem {
 
 class ProductList {
 	products = [
-		new Product("A Pillow", "https://imgs.search.brave.com/emwM74thqCauE0n4EshcBiHJnq1_ZNXl3F043gWID_8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/cGFjaWZpY2NvYXN0/LmNvbS9vbi9kZW1h/bmR3YXJlLnN0YXRp/Yy8tL1NpdGVzLXBj/Zi1tYXN0ZXItY2F0/YWxvZy9kZWZhdWx0/L2R3NjE0MTU2Nzcv/aW1hZ2VzL1BpbGxv/d3MvcGFjaWZpYy1j/b2FzdC1sdXh1cnkt/ZG93bi1maXJtLXBp/bGxvdy0yNjQ3Ni03/Ny5qcGc", "A nice carpet!", 39.99),
-		new Product("A Carpet", "https://imgs.search.brave.com/Yae0XrGoqQz3DuWbH_5nUW57jX2P451RHe3d4NhMOuQ/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC43/NW1iWDk1aDhOa1Zx/czBFc3VCZUtnSGFI/YSZwaWQ9QXBp", "A soft pillow!", 19.99),
+		new Product("A Carpet", "https://imgs.search.brave.com/Yae0XrGoqQz3DuWbH_5nUW57jX2P451RHe3d4NhMOuQ/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC43/NW1iWDk1aDhOa1Zx/czBFc3VCZUtnSGFI/YSZwaWQ9QXBp", "A nice carpet!", 39.99),
+		new Product("A Pillow", "https://imgs.search.brave.com/emwM74thqCauE0n4EshcBiHJnq1_ZNXl3F043gWID_8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/cGFjaWZpY2NvYXN0/LmNvbS9vbi9kZW1h/bmR3YXJlLnN0YXRp/Yy8tL1NpdGVzLXBj/Zi1tYXN0ZXItY2F0/YWxvZy9kZWZhdWx0/L2R3NjE0MTU2Nzcv/aW1hZ2VzL1BpbGxv/d3MvcGFjaWZpYy1j/b2FzdC1sdXh1cnkt/ZG93bi1maXJtLXBp/bGxvdy0yNjQ3Ni03/Ny5qcGc", "A soft pillow!", 19.99),
 	];
 
 	constructor() {}
@@ -85,12 +126,11 @@ class Shop {
 
 	render() {
 		const renderHook = document.getElementById("app");
-		this.cart = new ShoppingCart();
-		const cartEl = this.cart.render();
+		this.cart = new ShoppingCart("app");
+		this.cart.render();
 		const productList = new ProductList();
 		const prodListEl = productList.render();
 		
-		renderHook.append(cartEl);
 		renderHook.append(prodListEl);
 
 	}
@@ -98,7 +138,7 @@ class Shop {
 
 class App {
 	static cart;
-	
+
 	static init() {
 		const shop = new Shop();
 		shop.render();
